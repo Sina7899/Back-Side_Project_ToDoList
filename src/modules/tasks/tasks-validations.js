dcadee7df1a940c9e5e4aabd4bff408fc36cb263
Sina <sinasamii7878@gmail.com>
2024-08-18 11:50:28 +0330

    Make multi parameter validator middlewares to return multi error and add those Joi schema name with body suffix

--- src/modules/tasks/tasks-validations.js
+++ src/modules/tasks/tasks-validations.js
@@ -16,37 +16,42 @@ const tasksInfoByUserIdValidator = async (req, res, next) => {
 };
 
 const createTaskValidator = async (req, res, next) => {
-  try {
-    const paramsSchema = Joi.object({
-      userId: Joi.number().required(),
-      title: Joi.string().required(),
-      description: Joi.string().required(),
-    }).required();
+  const bodySchema = Joi.object({
+    userId: Joi.number().required(),
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+  }).required();
 
-    const validatedParams = await paramsSchema.validateAsync(req.params);
+  try {
+    const validatedParams = await bodySchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
     req.validatedParams = validatedParams;
-    console.log(validatedParams);
     next();
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    const errorMessages = error.details.map((detail) => detail.message);
+    res.status(400).json({ errors: errorMessages });
   }
 };
 
 const updateTaskValidator = async (req, res, next) => {
   try {
-    const paramsSchema = Joi.object({
+    const bodySchema = Joi.object({
       taskId: Joi.number().required(),
       title: Joi.string().required(),
       description: Joi.string().required(),
       status: Joi.boolean().required(),
     }).required();
 
-    const validatedParams = await paramsSchema.validateAsync(req.params);
+    const validatedParams = await bodySchema.validateAsync(req.body, {
+      abortEarly: false,
+    });
     req.validatedParams = validatedParams;
     console.log(validatedParams);
     next();
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    const errorMessages = error.details.map((detail) => detail.message);
+    res.status(400).json({ errors: errorMessages });
   }
 };
 
