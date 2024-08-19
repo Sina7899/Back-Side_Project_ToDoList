71890d1d5e1ed1a4ed1d73a8dcd14f9a8fc90f66
Sina <sinasamii7878@gmail.com>
2024-08-19 10:26:06 +0330

    Add Auth validator middleware for validating user JWTs

---
+++ src/core/middlewares/auth-middlewares.js
@@ -0,0 +1,26 @@
+import { jwtValidator } from "../auth/jwt-auth.js";
+
+const authValidatorMiddleware = (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new Error("Authorization header is missing!");
+    }
+
+    const userJwtToken = authHeader.split(" ")[1];
+
+    if (!userJwtToken) {
+      throw new Error("JWT Token is missing!");
+    }
+
+    const tokenData = jwtValidator(userJwtToken);
+    req.userTokenData = tokenData;
+
+    next();
+  } catch (error) {
+    res.status(401).json({ message: error.message });
+  }
+};
+
+export { authValidatorMiddleware };
