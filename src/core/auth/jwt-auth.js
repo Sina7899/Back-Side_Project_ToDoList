30129ef3b0e07ba46cb74135ffe3f2ead56d720f
Sina <sinasamii7878@gmail.com>
2024-08-19 12:23:18 +0330

    Remove JWT expire time

--- src/core/auth/jwt-auth.js
+++ src/core/auth/jwt-auth.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import { JWT_SECRETS } from "../secrets/secrets.js";
 
 function jwtSign(dataObject) {
-  return jwt.sign(dataObject, JWT_SECRETS.signKey, { expiresIn: "60m" });
+  return jwt.sign(dataObject, JWT_SECRETS.signKey);
 }
 
 function jwtValidator(jwtToken) {
