722a252a6ddb3cc0be2aad785c4f7eff54c11a42
Sina <sinasamii7878@gmail.com>
2024-08-19 10:30:31 +0330

    Add User create and loginValidator  controller middlewares

---
+++ src/modules/users/users-controllers.js
@@ -0,0 +1,48 @@
+import {
+  createUserService,
+  validateUserLoginService,
+} from "../../services/users/users-services.js";
+
+import "dotenv/config";
+
+const TABLE = process.env.TABLE_2;
+
+const createUserController = async (req, res, next) => {
+  try {
+    const { firstName, lastName, username, password } = req.validatedBody;
+    const createdTask = await createUserService(
+      TABLE,
+      firstName,
+      lastName,
+      username,
+      password
+    );
+    if (createdTask === null) {
+      res.status(500).json({
+        message: `"User Create Failed!"`,
+      });
+    } else {
+      res.status(201).json({ message: "User created successfully." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const loginUserController = async (req, res, next) => {
+  try {
+    const { username, password } = req.validatedBody;
+    const jwt = await validateUserLoginService(TABLE, username, password);
+    res.status(200).json({ jwt: jwt });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({
+      message: error.message,
+    });
+  }
+};
+
+export { createUserController, loginUserController };
