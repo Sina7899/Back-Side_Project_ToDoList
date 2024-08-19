4701d69ace08b562bc3a1e2d33eab171c304e1e6
Sina <sinasamii7878@gmail.com>
2024-08-19 10:23:53 +0330

    Add generate JTW and JWT validator functions with jwt package

---
+++ src/core/auth/jwt-auth.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+import { JWT_SECRETS } from "../secrets/secrets.js";
+
+function jwtSign(dataObject) {
+  return jwt.sign(dataObject, JWT_SECRETS.signKey, { expiresIn: "60m" });
+}
+
+function jwtValidator(jwtToken) {
+  try {
+    const verifiedJWT = jwt.verify(jwtToken, JWT_SECRETS.signKey);
+    return verifiedJWT;
+  } catch (error) {
+    console.log(error);
+    throw new Error("Invalid JWT Token!");
+  }
+}
+
+export { jwtSign, jwtValidator };
